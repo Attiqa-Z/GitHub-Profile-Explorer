14abc3cf7b5895660eead5168013e7d18970bb5b
Attiqa-Z <attiqazaki1@gmail.com>
2025-02-26 12:47:55 +0500

    Change this

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -3,8 +3,8 @@ import { Box, Card, CardContent, Typography } from "@mui/material";
 const Cards = () => {
 
   return (
-          <Box width="auto" height='auto' sx={{ border: 1, display: "block"  }}>
-          <Box width="600px" height='100px' sx={{ border: 1, margin: "10px" }}>
+          <Box width="auto" height='auto' sx={{ border: 2, display: "block"  }}>
+          <Box width="600px" height='100px' sx={{ border: 2, margin: "10px" }}>
         <Card sx={{ position: "relative", height:'100px' }}>
           <CardContent >
             <Typography
